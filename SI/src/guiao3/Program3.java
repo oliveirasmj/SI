44222489826c2346411d427f8fb3ceb77b3244e1
Miguel Oliveira <oliveirasmj@gmail.com>
2020-11-17 21:54:43 +0000

    insert

--- SI/src/guiao3/Program3.java
+++ SI/src/guiao3/Program3.java
@@ -25,17 +25,16 @@ public class Program3 {
 		try {
 			Scanner sc = new Scanner(System.in);
 			
-			//Caminho do ficheiro
-			System.out.println("Insira o nome do ficheiro para guardar a chave: ");
-			String fileName = sc.nextLine();
-			String path = "C:\\Users\\miguel.oliveira\\Downloads\\" + fileName +".txt"; // caminho final para o arquivo
-			
 			//Gerar a chave
 			KeyGenerator keygenerator = KeyGenerator.getInstance("DES"); //implementa um gerador de chaves simétricas --> DES - cifra simetrica por blocos
 			SecretKey chaveDES = keygenerator.generateKey(); //implementa uma chave simétrica
-			byte[] lines = chaveDES.getEncoded();
-			System.out.println();
+			byte[] lines = chaveDES.getEncoded(); //guardar chave em: lines -> para remover depois
 			
+			
+			//Escolher caminho do ficheiro para guardar a chave
+			System.out.print("Insira o nome do ficheiro para guardar a chave: ");
+			String fileName = sc.nextLine();
+			String path = "C:\\Users\\miguel.oliveira\\Downloads\\" + fileName +".txt"; // caminho final para o arquivo
 			//Escrever a chave no ficheiro
 			try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) { //true - indicar que não é para recriar arquivo
 				for(byte line : lines) { 	//para cada linha do array
@@ -45,14 +44,17 @@ public class Program3 {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-
+			
+	
+			
 			// Cria a cifra
 			Cipher cifraDES = Cipher.getInstance("DES/ECB/PKCS5Padding"); //Cipher - permite realizar operações de cifra e decifra
 			// Inicializa a cifra para o processo de ENCRIPTACAO
 			cifraDES.init(Cipher.ENCRYPT_MODE, chaveDES); //ENCRYPT_MODE
+			
 
-			//Ficheiro a encriptar
-			System.out.println("Qual o ficheiro a encriptar: ");
+			//Escolher ficheiro a encriptar e guardar na variavel allLines
+			System.out.print("Introduza o nome do ficheiro a encriptar: ");
 			fileName = sc.nextLine();
 			String ficheiroTextoClaro = "C:\\Users\\miguel.oliveira\\Downloads\\" + fileName + ".txt"; //caminho do arquivo
 			FileReader fr = null;
@@ -77,41 +79,45 @@ public class Program3 {
 				System.out.println("Error: " + e.getMessage());
 			}
 			
-			// Ir buscar o ficheiro da chave
-			System.out.println("Introduza o nome do ficheiro da chave: ");
+			
+			// Ir buscar o ficheiro da chave e guardar a chave em: chave
+			System.out.print("Introduza o nome do ficheiro da chave: ");
 			fileName = sc.nextLine();
 			String ficheiroChave = "C:\\Users\\miguel.oliveira\\Downloads\\" + fileName + ".txt"; //caminho do arquivo
 			fr = null;
 			br = null;
 			
+			String chave = "";
 			try {
 				fr = new FileReader(ficheiroChave); //instanciar filereader com o caminho do arquivo
 				br = new BufferedReader(fr); //br = new BufferedReader(new FileReader(path))
+				StringBuilder sb = new StringBuilder();
 				
 				String line = br.readLine(); //se arquivo estiver no final é devolvido nulo
 				
 				while(line != null) { //enquanto nao estiver no final
-					System.out.println(line); //escrever a linha
+					//System.out.println(line); //escrever a linha
+					sb.append(line);
 					line = br.readLine();	//ler a linha
 				}
+				chave = sb.toString();
 				
 			} catch (IOException e) {
 				System.out.println("Error: " + e.getMessage());
 			}
 			
-			//--------------------------------------------------------------------
-			//byte[] textoPuro = "Exemplo de texto puro".getBytes();
-			byte[] textoPuro = allLines.getBytes();
-			System.out.println("Texto [Formato de Byte] : " + textoPuro);
-			System.out.println("Texto Puro : " + new String(textoPuro));
-
-			// Texto encriptado
-			byte[] textoEncriptado = cifraDES.doFinal(textoPuro);
+			
+			//Encriptar o texto allLines
+			System.out.println();
+			byte[] textoClaro = allLines.getBytes();
+			System.out.println("Texto [Formato de Byte] : " + textoClaro);
+			System.out.println("Texto Puro : " + new String(textoClaro));
+			byte[] textoEncriptado = cifraDES.doFinal(textoClaro);
 			System.out.println("Texto Encriptado : " + textoEncriptado);
 
+			
 			// Inicializa a cifra também para o processo de DESINCRIPTACAO
 			cifraDES.init(Cipher.DECRYPT_MODE, chaveDES); //DECRYPT_MODE
-
 			// Decriptografa o texto
 			byte[] textoDecriptografado = cifraDES.doFinal(textoEncriptado);
 			System.out.println("Texto Decriptografado : " + new String(textoDecriptografado));
